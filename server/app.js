8381219ca90bc1e72f7b7aa1091643ec82262e93
Kris Szafranski <kdszafranski@gmail.com>
2016-08-30 10:10:54 -0500

    packaged app in node

---
+++ server/app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Serve back static files
+app.use(express.static(path.join(__dirname, './public')));
+
+// Handle index file separately
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, './public/views/index.html'));
+})
+
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function() {
+    console.log('Listening on port: ', app.get('port'));
+});
